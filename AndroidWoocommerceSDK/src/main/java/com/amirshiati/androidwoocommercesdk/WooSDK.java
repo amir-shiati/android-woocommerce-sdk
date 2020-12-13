2ab00fabf59e3e99900ff1a4c57037921a40b8b3
amir shiati <amirshiati98@gmail.com>
2020-12-13 11:52:06 +0330

    init

---
+++ AndroidWoocommerceSDK/src/main/java/com/amirshiati/androidwoocommercesdk/WooSDK.java
@@ -0,0 +1,11 @@
+package com.amirshiati.androidwoocommercesdk;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class WooSDK {
+
+    public static void makeToast(Context context) {
+        Toast.makeText(context, "Toast", Toast.LENGTH_SHORT).show();
+    }
+}
